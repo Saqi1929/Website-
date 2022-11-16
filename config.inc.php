ad8e096f72e9e522d2fa2acc680a61fac7945ae4
Saqi1929 <117446937+Saqi1929@users.noreply.github.com>
2022-11-16 23:47:38 +0400

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,14 @@
+<?php
+define("HOSTNAME_MTA_PUBLISH", "sdbemta04.simdif.local");
+define("EMAIL_CONFIRMED", true);
+define("SITE_URL", "https://www.saqlainali.com");
+define("SENDER_EMAIL", "contact@simdif.com");
+define("SENDER_NAME", "contact");
+define("RECEIVER_EMAIL", "itsmesaqi2@gmail.com");
+define("RECEIVER_NAME", "Saqlain Ali");
+define("RECEIVER_LASTNAME", "Saqi");
+define("SUBJECT", "[sender_email] via [site_name]: [form_subject]");
+define("CONTENT", "You have a message from &lt;[sender_email]&gt;, via your contact page on [site_name]");
+define("USER_ID", "1630921");
+define("SITE_ID", "1802083");
+define("ENV", "prod");
